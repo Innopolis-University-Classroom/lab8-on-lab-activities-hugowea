3259d8c8c9ea3850561604c6e8ece1c45c4a0868
hugowea <88401434+hugowea@users.noreply.github.com>
2022-03-21 15:09:17 +0300

    Update Cart.java

--- src/com/muwaffaq/innopolis/solid/SRP/right/Cart.java
+++ src/com/muwaffaq/innopolis/solid/SRP/right/Cart.java
@@ -1,1 +1,25 @@
+package com.muwaffaq.innopolis.solid.SRP.wrong;
+import com.muwaffaq.innopolis.solid.SRP.correct.Product;
 
+import java.util.List;
+
+public class Cart {
+    List<Product> products;
+    double totals;
+    String token;
+    void applyDiscount(int percentage) {
+        totals = totals * percentage;
+    }
+}
+
+public class AddToCart {
+    void addToCart(Product product) {
+        products.add(product);
+    }
+}
+
+public class RemoveFromCart {
+    void removeFromCart(Product product) {
+        products.remove(product);
+    }
+}
