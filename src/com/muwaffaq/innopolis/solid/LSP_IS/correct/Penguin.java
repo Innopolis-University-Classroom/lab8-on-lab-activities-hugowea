82b1ab838fe7417d9a681a56e55151b69d61ceff
hugowea <88401434+hugowea@users.noreply.github.com>
2022-03-21 15:49:17 +0300

    Create Penguin.java

---
+++ src/com/muwaffaq/innopolis/solid/LSP_IS/correct/Penguin.java
@@ -0,0 +1,23 @@
+package com.muwaffaq.innopolis.solid.LSP_IS.wrong;
+
+
+public class Penguin implements Bird, BirdSwim {
+
+    @Override
+    public void eat() {
+        System.out.println("eating");
+    }
+
+    @Override
+    public void hatch() {
+        System.out.println("laying eggs");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("swim");
+    }
+
+
+}
+© 2022 GitHub, Inc.
