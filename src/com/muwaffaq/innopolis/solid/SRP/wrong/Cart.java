5e9ba4c4eea523b5e394c1d2425ea2443cc045cb
hugowea <88401434+hugowea@users.noreply.github.com>
2022-03-21 14:59:55 +0300

    Update Cart.java

--- src/com/muwaffaq/innopolis/solid/SRP/wrong/Cart.java
+++ src/com/muwaffaq/innopolis/solid/SRP/wrong/Cart.java
@@ -3,35 +3,30 @@ import com.muwaffaq.innopolis.solid.SRP.correct.Product;
 
 import java.util.List;
 
-/**
- * ❌ ❌ ❌ ❌
- * how many responsibilities does it have?
- *
- * SRP :
- * a class should only have one responsibility
- * it should only have one reason to change
- */
-
 public class Cart {
-
     List<Product> products;
     double totals;
     String token;
+}
 
+public class AddToCart {
     void addToCart(Product product) {
         products.add(product);
     }
+}
 
+public class RemoveFromCart {
     void removeFromCart(Product product) {
         products.remove(product);
     }
+}
 
+public class ApplyDiscount {
     void applyDiscount(int percentage) {
         totals = totals * percentage;
     }
-
-
 }
+
 
 
 
